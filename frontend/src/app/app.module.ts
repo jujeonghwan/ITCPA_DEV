72568fd4c404875fcec255a7ed030d3c0649ccbd
jujeonghwan <jujeonghwan@gmail.com>
2022-02-01 13:42:24 -0500

    Added frontend initial app

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateComponent,
+    ReadComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
